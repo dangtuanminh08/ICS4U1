de87d463e780fe9fb36d9e6a961295deaefebc99
dangtuanminh08 <dangtuanminh1123@gmail.com>
2026-02-25 14:02:05 -0500

    Finished Feb 18

--- Unit 1/Feb18.java
+++ Unit 1/Feb18.java
@@ -6,6 +6,25 @@ This program contains the solutions to Feb 18 tasks
 
 public class Feb18 {
     public static void main(String args[]){
-        
+        System.out.printf( "%3d\n%-3d\n%03d\n", 7, 7, 7 );
+        // Task 1
+        /*
+        public class FancyStuff {
+            public static void main( String[] args )
+            {
+                System.out.printf( "%12s Java\n", "Welcome to" );   Adds 12 spaces before "Welcome to"     
+                System.out.printf( "%-12s Java\n", "Welcome to" );   Adds 12 spaces after "Welcome to"
+                System.out.printf( "%3s Java\n", "Welcome to" );   Adds 3 spaces before "Welcome to"
+                System.out.printf( "%.3s Java\n", "Welcome to" );   Only prints 3 characters of "Welcome to", so "Wel"
+                System.out.printf( "%3d\n%-3d\n%03d\n", 7, 7, 7 );  Adds 3 spaces before 7, adds 3 spaces after another 7, makes 7 a three-character long string with placeholder 0s 
+                System.out.printf( "%.3f\n%9.3f\n%1.2f\n", 123.94536, 123.94536, 123.94536 );        
+            }    
+        }
+        */
+
+        //Task 2
+        System.out.printf("%s\n%s%2s\n%s%3s\n%s%2s\n%s", "*", "*", "*", "*", "*", "*", "*", "*");
+
+        //Task 3 and 4 are done in February 19.
     }
 }
